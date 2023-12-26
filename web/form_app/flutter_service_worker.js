73df89b706312a20ce3a5347cd25580468b2fc9a
domesticmouse <domesticmouse@users.noreply.github.com>
2023-12-26 04:25:06 +0000

    deploy: 591470aafc7183bc6483135da46e9b8a3eb947ce

--- web/form_app/flutter_service_worker.js
+++ web/form_app/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "abaeea879f3279d2833ea9b6d03fcc4f",
-"index.html": "6163b029d5154f8b083e20ccb559e6c6",
-"/": "6163b029d5154f8b083e20ccb559e6c6",
+"index.html": "91a46d46a9bd6a896e433011c57da252",
+"/": "91a46d46a9bd6a896e433011c57da252",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
--- web/simplistic_editor/flutter_service_worker.js
+++ web/simplistic_editor/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "fcc4cffd0f45ba41f31bdd5e05ca1ded",
-"index.html": "91136af87cd7ad62c9db9a4f2fb484d3",
-"/": "91136af87cd7ad62c9db9a4f2fb484d3",
+"index.html": "33211ddbb54272d7e927cee63a1077b7",
+"/": "33211ddbb54272d7e927cee63a1077b7",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/fonts/MaterialIcons-Regular.otf": "3157eafc066cfa151439d4a7368bb3b3",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
