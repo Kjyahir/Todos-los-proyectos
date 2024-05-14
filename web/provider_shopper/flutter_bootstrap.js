0184902f54552f810b715cb22d59594b44840437
ericwindmill <ericwindmill@users.noreply.github.com>
2024-05-14 15:49:35 +0000

    deploy: be5290689436610d24df95e8a91f24c51e816de3

--- web/provider_shopper/flutter_bootstrap.js
+++ web/provider_shopper/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2642721874"
+    serviceWorkerVersion: "1733039493"
   }
 });
