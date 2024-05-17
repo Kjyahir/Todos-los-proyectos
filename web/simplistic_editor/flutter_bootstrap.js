08666d49c9b0ac9ef0c52fbcc5762fe035620e2c
parlough <parlough@users.noreply.github.com>
2024-05-17 15:33:00 +0000

    deploy: e5f6d45ea291fc23e553a0ec0baaf4b239b2ea80

--- web/simplistic_editor/flutter_bootstrap.js
+++ web/simplistic_editor/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1673825443"
+    serviceWorkerVersion: "4186530101"
   }
 });
