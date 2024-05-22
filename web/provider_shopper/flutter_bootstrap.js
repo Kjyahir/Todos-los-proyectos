0f6a67abcd4609c3db8d2e12b699ea2d21b1b10b
parlough <parlough@users.noreply.github.com>
2024-05-22 18:29:59 +0000

    deploy: 649e0e4db96afcf3c64320b70a36a17d19373522

--- web/provider_shopper/flutter_bootstrap.js
+++ web/provider_shopper/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2674780520"
+    serviceWorkerVersion: "3309327108"
   }
 });
