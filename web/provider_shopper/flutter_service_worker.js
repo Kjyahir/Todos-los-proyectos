5d7d60c0ca566b6b5078215ddf500c653a29530e
domesticmouse <domesticmouse@users.noreply.github.com>
2024-06-05 09:19:31 +0000

    deploy: daa4aed403e637c49815c4d1dcec1c15264c0e04

--- web/provider_shopper/flutter_service_worker.js
+++ web/provider_shopper/flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "8cd9b0527efb8828cd336f4a2bdd731e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "3c5379b625d9493f0292fa690ef30a9c",
+"flutter_bootstrap.js": "48fd3afb54112cb370d07b0c32d8e92d",
 "version.json": "c877dd821e537c427c816dfd698efafd",
 "index.html": "ada70000927cee2c3ff532cc109c6924",
 "/": "ada70000927cee2c3ff532cc109c6924",
